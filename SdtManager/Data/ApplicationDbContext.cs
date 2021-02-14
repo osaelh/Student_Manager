b1346a281d47c6df2694903d0ff28107f5f540ed
osaelh <oussamaelh@gmail.com>
2021-02-14 18:04:13 +0100

    Student crud operations

---
+++ SdtManager/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using SdtManager.Models;
+
+namespace SdtManager.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<SdtManager.Models.Student> Student { get; set; }
+    }
+}
---
+++ SdtManager/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SdtManager.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string CIN { get; set; }
+        public string Adress { get; set; }
+    }
+}
